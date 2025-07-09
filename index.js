4f690965de350b41d89e10ff5bc094970e7359a4
Zhihua Li <e925874478@gmail.com>
2025-07-09 14:59:53 -0400

    finish buttons next

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-var playList = [];
 var player = new Audio("sounds/crash.mp3");
 player.play();
 
+const playList = [
+  { title: "Song 1", artist: "Artist 1", src: "song1.mp3" },
+  { title: "Song 2", artist: "Artist 2", src: "song2.mp3" },
+  { title: "Song 3", artist: "Artist 3", src: "song3.mp3" },
+];
+
 var randomNumber = Math.floor(Math.random() * playList.length) + 1;
+console.log(randomNumber);
+
+$(".shuffle-btn").click(function () {});
+$(".prev - btn").click(function () {});
+$(".play-pause").click(function () {});
+$(".next-btn").click(function () {});
+$(".repeat-btn").click(function () {});
